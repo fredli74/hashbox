b2cc4b34987b522579cb07fddaa481ee47205e82
Fredrik Lidström <fredli74@gmail.com>
2015-12-02 23:43:12 +0100

    Fixed typo in build_all script

--- build_all.sh
+++ build_all.sh
@@ -4,14 +4,14 @@ revision=`git describe HEAD --always`
 
 cd server
 rm "$GOPATH/bin/hashbox/"*
-GOOS=darwin go build -ldflags "-X main.Version=$revision -X" -o "$GOPATH/bin/hashbox/hashbox-mac"
-GOOS=windows go build -ldflags "-X main.Version=$revision -X" -o "$GOPATH/bin/hashbox/hashbox-windows.exe"
-GOOS=freebsd go build -ldflags "-X main.Version=$revision -X" -o "$GOPATH/bin/hashbox/hashbox-freebsd-amd64"
-GOARM=7 GOOS=linux GOARCH=arm go build -ldflags "-X main.Version=$revision -X" -o "$GOPATH/bin/hashbox/hashbox-linux-armv7l"
+GOOS=darwin go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-mac"
+GOOS=windows go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-windows.exe"
+GOOS=freebsd go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-freebsd-amd64"
+GOARM=7 GOOS=linux GOARCH=arm go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-linux-armv7l"
 echo $GOARM
 cd ../hashback
-GOOS=darwin go build -ldflags "-X main.Version=$revision -X" -o "$GOPATH/bin/hashbox/hashback"
-GOOS=windows go build -ldflags "-X main.Version=$revision -X" -o "$GOPATH/bin/hashbox/hashback.exe"
+GOOS=darwin go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashback"
+GOOS=windows go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashback.exe"
 
 #-ldflags "-s"  for "release" version
 
