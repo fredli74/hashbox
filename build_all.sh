bc7b42276b7d8c41fc068f22090abaceea98b0da
Fredrik Lidström <fredli74@gmail.com>
2016-05-09 23:29:26 +0200

    Remade a new build_all script and added new build platforms

--- build_all.sh
+++ build_all.sh
@@ -1,27 +1,27 @@
+#!/bin/sh
 set -x
 version=`git describe HEAD --always --abbrev=0`
 revision=`git describe HEAD --always`
 
-cd server
+rm "$GOPATH/bin/hashbox/"*.zip
 rm "$GOPATH/bin/hashbox/"*
-GOOS=darwin go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-mac"
-GOOS=windows go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-windows.exe"
-GOOS=freebsd go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-freebsd-amd64"
-GOARM=7 GOOS=linux GOARCH=arm go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashbox-linux-armv7l"
-echo $GOARM
+
+BuildAndZip () {
+	rm "$GOPATH/bin/hashbox/$2"
+	go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/$2"
+	rm "$GOPATH/bin/hashbox/$1-$version.zip"
+	zip -jD "$GOPATH/bin/hashbox/$1-$version.zip" "$GOPATH/bin/hashbox/$2"
+}
+
+cd server
+GOOS=darwin  GOARCH=amd64       BuildAndZip "hashbox-mac-amd64"     "hashbox-mac"
+GOOS=windows GOARCH=amd64       BuildAndZip "hashbox-windows-amd64" "hashbox-windows.exe"
+GOOS=freebsd GOARCH=amd64       BuildAndZip "hashbox-freebsd-amd64" "hashbox-freebsd-amd64"
+GOOS=linux   GOARCH=arm GOARM=7 BuildAndZip "hashbox-linux-armv7l"  "hashbox-linux-armv7l"
 cd ../hashback
-GOOS=darwin go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashback"
-GOOS=windows go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashback.exe"
-GOOS=windows GOARCH=386 go build -ldflags "-X main.Version=$revision" -o "$GOPATH/bin/hashbox/hashback-x86.exe"
+GOOS=linux   GOARCH=amd64 BuildAndZip "hashback-linux-amd64" "hashback"
+GOOS=darwin  GOARCH=amd64 BuildAndZip "hashback-mac-amd64"   "hashback"
+GOOS=windows GOARCH=amd64 BuildAndZip "hashback-win-amd64"   "hashback.exe"
+GOOS=windows GOARCH=386   BuildAndZip "hashback-win-x86"     "hashback-x86.exe"
 
-#-ldflags "-s"  for "release" version
-
-cd $GOPATH/bin/hashbox/
-rm *.zip
-zip -D "hashbox-mac-amd64-$version.zip"     "hashbox-mac"
-zip -D "hashbox-windows-amd64-$version.zip" "hashbox-windows.exe"
-zip -D "hashbox-freebsd-amd64-$version.zip" "hashbox-freebsd-amd64"
-zip -D "hashbox-linux-armv7l-$version.zip"  "hashbox-linux-armv7l"
-zip -D "hashback-mac-amd64-$version.zip"    "hashback"
-zip -D "hashback-win-amd64-$version.zip"    "hashback.exe"
-zip -D "hashback-win-x86-$version.zip"      "hashback-x86.exe"
+exit 0
