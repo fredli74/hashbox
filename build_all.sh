acdfa0685f027afef6d3fe1597d1984fe7756fcd
Fredrik Lidström <fredli74@gmail.com>
2026-01-07 12:13:59 +0100

    Adding hashbox-util build to build_all

--- build_all.sh
+++ build_all.sh
@@ -21,12 +21,19 @@ GOOS=linux   GOARCH=arm GOARM=7 BuildAndZip "hashbox-linux-armv7l"  "hashbox-lin
 GOOS=darwin  GOARCH=amd64       BuildAndZip "hashbox-mac-amd64"     "hashbox-mac-amd64"
 GOOS=darwin  GOARCH=arm64       BuildAndZip "hashbox-mac-arm64"     "hashbox-mac"
 GOOS=windows GOARCH=amd64       BuildAndZip "hashbox-windows-amd64" "hashbox-windows.exe"
+cd ../util
+go fmt
+GOOS=freebsd GOARCH=amd64       BuildAndZip "hashbox-util-freebsd-amd64" "hashbox-util-freebsd-amd64"
+GOOS=linux   GOARCH=arm GOARM=7 BuildAndZip "hashbox-util-linux-armv7l"  "hashbox-util-linux-armv7l"
+GOOS=darwin  GOARCH=amd64       BuildAndZip "hashbox-util-mac-amd64"     "hashbox-util-mac-amd64"
+GOOS=darwin  GOARCH=arm64       BuildAndZip "hashbox-util-mac-arm64"	 "hashbox-util-mac"
+GOOS=windows GOARCH=amd64       BuildAndZip "hashbox-util-windows-amd64" "hashbox-util-windows.exe"
 cd ../hashback
 go fmt
-GOOS=linux   GOARCH=amd64 BuildAndZip "hashback-linux-amd64" "hashback"
-GOOS=darwin  GOARCH=amd64 BuildAndZip "hashback-mac-amd64"   "hashback-amd64"
-GOOS=darwin  GOARCH=arm64 BuildAndZip "hashback-mac-arm64"   "hashback"
-GOOS=windows GOARCH=amd64 BuildAndZip "hashback-win-amd64"   "hashback.exe"
-GOOS=windows GOARCH=386   BuildAndZip "hashback-win-x86"     "hashback-x86.exe"
+GOOS=linux   GOARCH=amd64       BuildAndZip "hashback-linux-amd64" "hashback"
+GOOS=darwin  GOARCH=amd64       BuildAndZip "hashback-mac-amd64"   "hashback-amd64"
+GOOS=darwin  GOARCH=arm64       BuildAndZip "hashback-mac-arm64"   "hashback"
+GOOS=windows GOARCH=amd64       BuildAndZip "hashback-win-amd64"   "hashback.exe"
+GOOS=windows GOARCH=386         BuildAndZip "hashback-win-x86"     "hashback-x86.exe"
 
 exit 0
